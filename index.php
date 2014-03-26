80ce875f00a801e04c3119978eeeb9b4f5081fbc
Brian FitzGerald <bmfitzgerald3@gmail.com>
2014-03-26 00:49:51 -0400

    uploader working

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Chris' File Uploader</title>
+</head>
+<body>
+
+	<form action="upload_file.php" method="post" enctype="multipart/form-data">
+
+
+		<label for="file">File</label>
+		<input type="file" name="file">
+
+		<br><br>
+
+		<input type="submit" name="submitBtn" value="upload">
+
+
+	</form>
+	
+</body>
+</html>
---
+++ upload_file.php
@@ -0,0 +1,53 @@
+<?php
+
+	// general variables
+	$allowedExts = array("gif", "jpeg", "jpg", "png");
+	$allowedTypes = array("image/gif", "image/jpeg", "image/pjpeg", "image/x-png", "image/png");
+	$tempFileName = $_FILES["file"]["tmp_name"];
+	$fileName = $_FILES["file"]["name"];
+	$fileType = $_FILES["file"]["type"];
+	$fileSize = $_FILES["file"]["size"];
+	$errorCode = $_FILES["file"]["error"];
+	$extension = end(explode(".", $fileName));
+	$uploadPath = "images/" . $fileName;
+
+	// validation checks
+	$isValidExtension = in_array($extension, $allowedExts);
+	$isValidType = in_array($fileType, $allowedTypes);
+	$isValidSize = $fileSize < 200000;
+	$hasErrorOccurred = $errorCode > 0;
+
+
+	if ($isValidType && $isValidSize && $isValidExtension) {
+
+		if ($hasErrorOccurred) {
+			echo("there was a problem with the upload");
+		}
+
+		else {
+
+			if (file_exists($uploadPath)) {
+				echo("this file already exists on the server");
+			}
+
+			else {
+
+				echo("everything looks good, place the file where we want it");
+
+				move_uploaded_file(
+					$tempFileName,
+					$uploadPath
+				);
+
+			}
+		}
+
+	}
+
+	else {
+		
+		echo("file doesn't meet validation criteria (i.e. invalid extension)");
+
+	}
+	
+?>
